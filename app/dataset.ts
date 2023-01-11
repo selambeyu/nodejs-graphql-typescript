452ccb3aee57f5e83df3530056d817911507287a
melkambeyene <melkam3392beyu@gmail.com>
2023-01-11 10:58:33 +0300

    initial  commit

---
+++ app/dataset.ts
@@ -0,0 +1,7 @@
+// create dummy database which will be used to send request data
+let people: { id: number; name: string }[] = [
+  { id: 1, name: "Cassie" },
+  { id: 2, name: "Rue" },
+  { id: 3, name: "Lexi" },
+];
+export default people;
